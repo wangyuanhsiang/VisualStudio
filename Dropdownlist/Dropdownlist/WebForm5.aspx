d2c71308cfa6bc843612d4a6252f4c9c95b0254b
wangyuanhsiang <ywang91@studentmail.nait.ca>
2015-09-27 23:54:34 -0600

    Modify DropdownList, Add QueryString
    database binding

---
+++ Dropdownlist/Dropdownlist/WebForm5.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm5.aspx.cs" Inherits="Dropdownlist.WebForm5" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:DropDownList ID="DropDownList1"    runat="server" DataTextField="CityName" DataValueField="CityId"></asp:DropDownList>
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" Text="Button" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ Dropdownlist/Dropdownlist/WebForm5.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Dropdownlist
+{
+    public partial class WebForm5 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                if (!IsPostBack)
+                {
+                    SqlCommand cmd = new SqlCommand("Select CityId, CityName, Country from tblCity", con);
+                    con.Open();
+                    DropDownList1.DataSource = cmd.ExecuteReader();
+                    //DropDownList1.DataTextField = "CityName";
+                    //DropDownList1.DataValueField = "CityId";
+                    DropDownList1.DataBind();
+                  
+                }
+                
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Write(" the Counrty is :" + DropDownList1.DataValueField.ToString());
+        }
+    }
+}
